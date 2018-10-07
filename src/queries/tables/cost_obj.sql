53b0090e2ba7145057783ea2cc554ae7163a162e
samuel <samuelschreiber96@gmail.com>
2018-10-07 00:21:14 -0700

    Add python script to construct db schema and add some test data

--- src/queries/tables/cost_obj.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cost_obj(
-name VARCHAR(50) NOT NULL,
-month VARCHAR(3) NOT NULL,
-year VARCHAR(4) NOT NULL,
-cost_obj_id serial NOT NULL,
-cost NUMERIC,
-user_id serial REFERENCES account(user_id),
-PRIMARY KEY (cost_obj_id, user_id)
-);
